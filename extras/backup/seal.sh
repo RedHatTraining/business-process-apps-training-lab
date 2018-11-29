70bb224691311ec8faccc0515ba3caaec4c8e4cd
root <root@foundation0.ilt.example.com>
2018-11-28 16:18:02 -0800

    remove tmp dir

--- extras/backup/seal.sh
+++ extras/backup/seal.sh
@@ -1,5 +1,10 @@
 ssh root@workstation 'echo -e "RHT_VENUE=ilt\nRHT_ROLE=workstation\nRHT_ENROLLMENT=\nRHT_COURSE=\nRHT_TITLE=\nRHT_VMTREE=rhel7.5/x86_64\nRHT_NETWORK=no" > /etc/rht'
+ssh root@workstation rm -rf /tmp/dc39
 ssh root@master "sed -i -e 's|^RHT_COURSE=.*|RHT_COURSE=|' /etc/rht"
+ssh root@master rm -rf /tmp/dc39
 ssh root@node1 "sed -i -e 's|^RHT_COURSE=.*|RHT_COURSE=|' /etc/rht"
+ssh root@node1 rm -rf /tmp/dc39
 ssh root@node2 "sed -i -e 's|^RHT_COURSE=.*|RHT_COURSE=|' /etc/rht"
+ssh root@node2 rm -rf /tmp/dc39
 ssh root@services "sed -i -e 's|^RHT_COURSE=.*|RHT_COURSE=|' /etc/rht"
+ssh root@services rm -rf /tmp/dc39
